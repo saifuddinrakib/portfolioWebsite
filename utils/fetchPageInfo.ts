89295dd8f625b448ec860e6b3918450cf6716ba1
saifuddinrakib <saifrakib1@gmail.com>
2023-09-09 22:06:52 +0600

    new

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,14 +1,28 @@
-
-
 import { PageInfo } from "../typings";
 
-export const fetchPageInfo = async() => {
+export const fetchPageInfo = async () => {
+  try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`);
 
+    if (!res.ok) {
+      throw new Error(`Failed to fetch pageInfo. Status: ${res.status}`);
+    }
+
     const data = await res.json();
+
+    if (!data || !data.pageInfo) {
+      throw new Error("Invalid data received from the API");
+    }
+
     const pageInfo: PageInfo = data.pageInfo;
 
-    // console.log('fetching', pageInfo);
+    // Optionally, you can log pageInfo for debugging purposes.
+    // console.log('Fetched pageInfo:', pageInfo);
 
     return pageInfo;
-}
+  } catch (error) {
+    // Handle errors gracefully, e.g., log the error or display a user-friendly message.
+    console.error("Error fetching pageInfo:", error);
+    throw error; // Rethrow the error to handle it further up the call stack.
+  }
+};
